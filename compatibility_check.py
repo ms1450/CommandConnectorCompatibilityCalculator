a9fcffbd9a37f49c4caae325718147f6797fec94
Ian Young <ian.young@verkada.com>
2024-08-26 13:56:48 -0600

    Linted

--- compatibility_check.py
+++ compatibility_check.py
@@ -7,7 +7,6 @@ Purpose: Import a list of third-party cameras and return to the terminal
 """
 
 import re
-from dataclasses import dataclass
 from typing import Dict, Set, List, Optional, Tuple, Union
 
 import colorama
@@ -26,35 +25,13 @@ from app.sanitize import (
     is_special_character,
     is_integer,
 )
-from app import log
+from app.output import print_results
+from app import log, CompatibleModel
 
 # Initialize colorized output
 colorama.init(autoreset=True)
 
 RETENTION = 30  # Required storage in days
-
-
-@dataclass
-class CompatibleModel:
-    """Represents a compatible model with its details.
-
-    Args:
-        model_name (str): The name of the model.
-        manufacturer (str): The manufacturer of the model.
-        minimum_supported_firmware_version (str): The minimum firmware
-            version that the model supports.
-        notes (str): Additional notes regarding the model.
-
-    Attributes:
-        channels (int): The number of channels associated with the model,
-            initialized to 0.
-    """
-
-    model_name: str
-    manufacturer: str
-    minimum_supported_firmware_version: str
-    notes: str
-    channels: int = 0
 
 
 def get_camera_list(
@@ -466,90 +443,6 @@ def camera_match(
     return traced_cameras
 
 
-def print_list_data(
-    customer_cameras: Dict[str, int],
-    traced_cameras: List[Tuple[str, str, Optional[CompatibleModel]]],
-):
-    """Print and save a formatted list of camera data.
-
-    Args:
-        customer_cameras (Dict[str, int]): A dictionary where keys are
-            camera names and values are counts.
-        traced_cameras
-        (List[Tuple[str, str, Optional[CompatibleModel]]]): A list of
-            tuples where each tuple contains the camera name, type, and
-            an Optional CompatibleModel.
-    """
-    output = []
-
-    for camera_name, camera_type, matched_camera in traced_cameras:
-        camera_count = str(customer_cameras.get(camera_name, 0))
-
-        # Append values to output table
-        output.append(
-            [
-                camera_name,
-                camera_count,
-                camera_type,
-                matched_camera.manufacturer if matched_camera else "",
-                matched_camera.model_name if matched_camera else "",
-                (
-                    matched_camera.minimum_supported_firmware_version
-                    if matched_camera
-                    else ""
-                ),
-                matched_camera.notes if matched_camera else "",
-            ]
-        )
-
-    # Create table headers
-    color_headers = [
-        f"{Fore.LIGHTBLACK_EX}Camera Name{Style.RESET_ALL}",
-        f"{Fore.LIGHTBLACK_EX}Count{Style.RESET_ALL}",
-        f"{Fore.LIGHTBLACK_EX}Match Type{Style.RESET_ALL}",
-        f"{Fore.LIGHTBLACK_EX}Manufacturer{Style.RESET_ALL}",
-        f"{Fore.LIGHTBLACK_EX}Model{Style.RESET_ALL}",
-        f"{Fore.LIGHTBLACK_EX}Min Firmware Version{Style.RESET_ALL}",
-        f"{Fore.LIGHTBLACK_EX}Notes{Style.RESET_ALL}",
-    ]
-
-    plain_headers = [
-        "Camera Name",
-        "Count",
-        "Match Type",
-        "Manufacturer",
-        "Model",
-        "Min Firmware Version",
-        "Notes",
-    ]
-
-    # Sort alphabetically
-    output.sort(key=lambda x: x[2], reverse=True)
-
-    # Print table in pretty format
-    print(tabulate(output, headers=color_headers, tablefmt="fancy_grid"))
-
-    # Convert to Pandas DataFrame
-    df = pd.DataFrame(
-        output,
-        columns=plain_headers,
-    )
-    # Strip color codes
-    df["Match Type"] = df["Match Type"].apply(strip_ansi_codes)
-
-    # NOTE: Uncomment to write truncated to terminal
-    # print(df.head())
-    # NOTE: Uncomment to write to html file
-    # df.to_html("camera_models.html", index=False)
-    # NOTE: Uncomment to write output to a csv
-    # with open("camera_models.txt", "w", encoding="UTF-8") as f:
-    #     f.write(
-    #         tabulate(
-    #             df.values.tolist(), headers=plain_headers, tablefmt="simple"
-    #         )
-    #     )
-
-
 def recommend_connectors(customer_cameras: Dict[str, int]):
     """
     Generate connector recommendations based on customer camera data and
@@ -627,7 +520,7 @@ def main():
             verkada_cameras,
             manufacturers,
         )
-        print_list_data(customer_cameras, traced_cameras)
+        print_results(customer_cameras, traced_cameras)
     else:
         log.critical(
             "%sCould not identify model column.%s", Fore.RED, Style.RESET_ALL
